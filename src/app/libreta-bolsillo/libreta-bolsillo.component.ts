43c3698e4cde05952aff1d268bc10185eb7488c6
jdelacruz07 <jdelacruz0077@gmail.com>
2020-04-22 17:14:09 +0200

    Se modifico bolsillo

--- src/app/libreta-bolsillo/libreta-bolsillo.component.ts
+++ src/app/libreta-bolsillo/libreta-bolsillo.component.ts
@@ -8,38 +8,11 @@ import { Component, OnInit, SystemJsNgModuleLoaderConfig } from '@angular/core';
 export class LibretaBolsilloComponent implements OnInit {
 
  fotos = [
-    //"/assets/IMG_4299.JPG",
-    //"/assets/IMG_4302.JPG",
-    //"/assets/IMG_4317.JPG",
+   
     {url: "/assets/IMG_4319.JPG", material: "Piel"},
     {url: "/assets/IMG_4326.JPG", material: "Tela"},
     {url: "/assets/IMG_4327.JPG", material: "Papel"}
   
-    /*"/assets/IMG_4338.JPG",
-    "/assets/IMG_4343.JPG",
-    "/assets/IMG_4346.JPG",
-    "/assets/IMG_4360.JPG",
-    "/assets/IMG_4365.JPG",
-    "/assets/IMG_4370.JPG",
-    "/assets/IMG_4375.JPG",
-    "/assets/IMG_4376.JPG",
-    "/assets/IMG_4378.JPG",
-    "/assets/IMG_4385.JPG",
-    "/assets/IMG_4387.JPG",
-    "/assets/IMG_4391.JPG",
-    "/assets/IMG_4392.JPG",
-    "/assets/IMG_4393.JPG",
-    "/assets/IMG_4394.JPG",
-    "/assets/IMG_4401.JPG",
-    "/assets/IMG_4402.JPG",
-    "/assets/IMG_4405.JPG",
-    "/assets/IMG_4406.JPG",
-    "/assets/IMG_4408.JPG",
-    "/assets/IMG_4409.JPG",
-    "/assets/IMG_4410.JPG",
-    "/assets/IMG_4411.JPG",
-    */
-
   ];
 
   constructor() { }
--- src/app/libreta-media-carta/libreta-media-carta.component.ts
+++ src/app/libreta-media-carta/libreta-media-carta.component.ts
@@ -13,39 +13,19 @@ export class LibretaMediaCartaComponent implements OnInit {
   }
 
 
- fotos = [
-  /*"/assets/IMG_4299.JPG",
-  "/assets/IMG_4302.JPG",
-  "/assets/IMG_4317.JPG",
-  "/assets/IMG_4319.JPG",
-  "/assets/IMG_4326.JPG",
-  "/assets/IMG_4327.JPG", 
-  {url:"/assets/IMG_4338.JPG", material: "Papel"},
-  {url:"/assets/IMG_4346.JPG", material: "Papel"},
-  {url:"/assets/IMG_4365.JPG", material: "Papel"},
-  {url:"/assets/IMG_4376.JPG", material: "Papel"},
-  {url:"/assets/IMG_4392.JPG", material: "Papel"},
-  {url:"/assets/IMG_4393.JPG", material: "Papel"},
-  {url:"/assets/IMG_4401.JPG", material: "Papel"},
-  {url:"/assets/IMG_4387.JPG", material: "Papel"},
-  */
-  {url:"/assets/IMG_4343.JPG", material: "Papel"},
-  {url:"/assets/IMG_4360.JPG", material: "Papel"},
-  {url:"/assets/IMG_4370.JPG", material: "Papel"},
-  {url:"/assets/IMG_4375.JPG", material: "Tela"},
-  {url:"/assets/IMG_4385.JPG", material: "Papel"},
-  {url:"/assets/IMG_4402.JPG", material: "Tela"},
-  {url:"/assets/IMG_4405.JPG", material: "Papel"},
-  {url:"/assets/IMG_4378.JPG", material: "Papel"},
-  {url:"/assets/IMG_4391.JPG", material: "Papel"},
-  {url:"/assets/IMG_4394.JPG", material: "Papel"},
-  /*
-  {url:"/assets/IMG_4406.JPG", material: "Papel"},
-  {url:"/assets/IMG_4408.JPG", material: "Papel"},
-  {url:"/assets/IMG_4409.JPG", material: "Papel"},
-  {url:"/assets/IMG_4410.JPG", material: "Papel"},
-  {url:"/assets/IMG_4411.JPG", material: "Papel"},*/
-  
+  fotos = [
+      
+    {url:"/assets/IMG_4343.JPG", material: "Papel"},
+    {url:"/assets/IMG_4360.JPG", material: "Papel"},
+    {url:"/assets/IMG_4370.JPG", material: "Papel"},
+    {url:"/assets/IMG_4375.JPG", material: "Tela"},
+    {url:"/assets/IMG_4385.JPG", material: "Papel"},
+    {url:"/assets/IMG_4402.JPG", material: "Tela"},
+    {url:"/assets/IMG_4405.JPG", material: "Papel"},
+    {url:"/assets/IMG_4378.JPG", material: "Papel"},
+    {url:"/assets/IMG_4391.JPG", material: "Papel"},
+    {url:"/assets/IMG_4394.JPG", material: "Papel"},
+    {url:"/assets/IMG_4317.JPG", material: "Papel"}  
+  ];
 
-];
 }
