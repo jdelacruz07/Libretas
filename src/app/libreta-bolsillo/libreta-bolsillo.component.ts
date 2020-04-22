fbbb667bf3dd29c3ae6f61926ae70365b1739a65
jdelacruz07 <jdelacruz0077@gmail.com>
2020-04-22 16:17:30 +0200

    Se corrige bolsillo

--- src/app/libreta-bolsillo/libreta-bolsillo.component.ts
+++ src/app/libreta-bolsillo/libreta-bolsillo.component.ts
@@ -11,9 +11,9 @@ export class LibretaBolsilloComponent implements OnInit {
     //"/assets/IMG_4299.JPG",
     //"/assets/IMG_4302.JPG",
     //"/assets/IMG_4317.JPG",
-    {url: "/assets/IMG_4319.JPG", material: "piel"},
-    {url: "/assets/IMG_4326.JPG", material: "tela"},
-    {url: "/assets/IMG_4327.JPG", material: "papel"}
+    {url: "/assets/IMG_4319.JPG", material: "Piel"},
+    {url: "/assets/IMG_4326.JPG", material: "Tela"},
+    {url: "/assets/IMG_4327.JPG", material: "Papel"}
   
     /*"/assets/IMG_4338.JPG",
     "/assets/IMG_4343.JPG",
