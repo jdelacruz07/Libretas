f672b1835d935f830ca0d746fc41ef030a7b6ec4
jdelacruz07 <jdelacruz0077@gmail.com>
2020-04-26 22:34:35 +0200

    Se corrige la imagen

--- src/app/libreta-media-carta/libreta-media-carta.component.ts
+++ src/app/libreta-media-carta/libreta-media-carta.component.ts
@@ -15,7 +15,7 @@ export class LibretaMediaCartaComponent implements OnInit {
 
   fotos = [
       
-    {url:"/assets/IMG_4317.JPG", material: "Papel"}  
+    {url:"/assets/IMG_4317.JPG", material: "Papel"},  
     {url:"/assets/IMG_4343.JPG", material: "Papel"},
     {url:"/assets/IMG_4360.JPG", material: "Papel"},
     {url:"/assets/IMG_4370.JPG", material: "Papel"},
@@ -25,7 +25,7 @@ export class LibretaMediaCartaComponent implements OnInit {
     {url:"/assets/IMG_4391.JPG", material: "Papel"},
     {url:"/assets/IMG_4394.JPG", material: "Papel"},
     {url:"/assets/IMG_4402.JPG", material: "Tela"},
-    {url:"/assets/IMG_4405.JPG", material: "Papel"},
+    {url:"/assets/IMG_4405.JPG", material: "Papel"}
   ];
 
 }
