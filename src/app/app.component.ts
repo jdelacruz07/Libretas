574bb8896adca258b83b212b141a28d25a508e29
jdelacruz07 <jdelacruz0077@gmail.com>
2020-04-27 00:26:39 +0200

    Se elimina arreglo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,36 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'yahailin';
-fotos = [
-/*"/assets/IMG_4299.JPG",
-  "/assets/IMG_4302.JPG",
-  "/assets/IMG_4317.JPG",
-  "/assets/IMG_4319.JPG",
-  "/assets/IMG_4326.JPG",
-  "/assets/IMG_4327.JPG", */
-  "/assets/IMG_4338.JPG",
-  "/assets/IMG_4343.JPG",
-  "/assets/IMG_4346.JPG",
-  "/assets/IMG_4360.JPG",
-  "/assets/IMG_4365.JPG",
-  "/assets/IMG_4370.JPG",
-  "/assets/IMG_4375.JPG",
-  "/assets/IMG_4376.JPG",
-  "/assets/IMG_4378.JPG",
-  "/assets/IMG_4385.JPG",
-  "/assets/IMG_4387.JPG",
-  "/assets/IMG_4391.JPG",
-  "/assets/IMG_4392.JPG",
-  "/assets/IMG_4393.JPG",
-  "/assets/IMG_4394.JPG",
-  "/assets/IMG_4401.JPG",
-  "/assets/IMG_4402.JPG",
-  "/assets/IMG_4405.JPG",
-  "/assets/IMG_4406.JPG",
-  "/assets/IMG_4408.JPG",
-  "/assets/IMG_4409.JPG",
-  "/assets/IMG_4410.JPG",
-  "/assets/IMG_4411.JPG"
-];
+  title = 'yailin';
+
 }
