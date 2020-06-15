5570609037527b86a43a39a7bf4b6f35ae796018
jdelacruz07 <jdelacruz0077@gmail.com>
2020-06-15 17:38:38 +0200

    Se genera segundo carrousel

--- src/app/libretas/libretas.component.ts
+++ src/app/libretas/libretas.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibretasComponent implements OnInit {
  
-  images = [4343, 4302, 4360].map((n) => `/assets/IMG_${n}.JPG`);
+  imageArtesanal = [4343, 4302, 4360].map((n) => `/assets/IMG_${n}.JPG`);
+  imageBolsillo = [4319, 4326, 4327].map((n) => `/assets/IMG_${n}.JPG`);
   
 
   constructor() { }
