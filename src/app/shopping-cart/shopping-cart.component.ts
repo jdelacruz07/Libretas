72e2d6c86a7a8f36c601376fea578fe1cce72387
jdelacruz07 <jdelacruz0077@gmail.com>
2020-06-30 18:51:37 +0200

    Se crea animacion

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { BuyNotebookService } from '../buy-notebook.service';
 import { notebook } from '../description/description.component';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  styleUrls: ['./shopping-cart.component.css'],
+  animations: [
+    trigger('myAnimation', [
+      state('inactive', style({
+        backgroundColor: '#eee'
+      })),
+      state('active', style({
+        backgroundColor: '#ffcc00'
+      })),
+
+      transition('* <=> void', [
+        style({
+          transform: 'translateX(80px)'
+        }),
+        animate('500ms ease-in')
+      ]),
+    ])
+  ]
 })
+
 export class ShoppingCartComponent implements OnInit {
   buyOfNotebooks: notebook[] = [];
   total: number = 0;
+  animationState = 'inactive';
 
   constructor(private buyNotebook: BuyNotebookService) { }
 
@@ -26,6 +46,7 @@ export class ShoppingCartComponent implements OnInit {
   deleteItem(index) {
     this.buyNotebook.deleteNotebook(index);
     this.getTotalPrice();
+    this.animationState = (this.animationState === 'inactive' ? 'active' : 'inactive');
   }
 
   getTotalPrice () {
